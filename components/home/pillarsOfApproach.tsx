45cef5593d40941e73b82b52236a59770223aa88
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-07-17 16:19:03 +0530

    Update pillarsOfApproach.tsx

--- components/home/pillarsOfApproach.tsx
+++ components/home/pillarsOfApproach.tsx
@@ -30,8 +30,12 @@ const PillarsOfApproach = () => {
   return (
     <section className="py-8 sm:py-10 px-2 sm:px-4 md:px-16">
       <Heading text="Pillars of Approach" />
-      <div className="grid gap-4 sm:gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+      {/* <div className="grid gap-4 sm:gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+        {pillars.map((pillar, index) => ( */}
+
+      <div className="mt-16 grid gap-16 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
         {pillars.map((pillar, index) => (
+
           <div
             key={index}
             className="relative h-[220px] sm:h-[400px] rounded-lg overflow-hidden group shadow-lg hover:shadow-xl transition-shadow duration-300 hover:scale-105 cursor-pointer transition-transform"
@@ -48,16 +52,17 @@ const PillarsOfApproach = () => {
             {/* Overlay */}
             <div className="absolute inset-0 bg-black/30">
               {/* Title */}
-              <div className="absolute bottom-2 sm:bottom-4 w-full text-center px-2 sm:px-4 transition-all duration-500 ease-in-out group-hover:opacity-0 group-hover:translate-y-5">
+              {/* <div className="absolute bottom-2 sm:bottom-4 w-full text-center px-2 sm:px-4 transition-all duration-500 ease-in-out group-hover:opacity-0 group-hover:translate-y-5">
                 <h3 className="text-white text-lg sm:text-4xl text-start font-semibold">{pillar.title}</h3>
-              </div>
+              </div> */}
 
-              {/* Description */}
-              <div className="absolute inset-0 flex items-center justify-center px-2 sm:px-4 text-center">
+              {/* Description */} {/* This need to be fixed later in animation*/}
+              {/* <div className="absolute inset-0 flex items-center justify-center px-2 sm:px-4 text-center">
                 <p className="text-white text-xs sm:text-xl max-w-xs opacity-0 translate-y-5 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-500 ease-in-out">
                   {pillar.description}
                 </p>
-              </div>
+              </div> */}
+
             </div>
           </div>
         ))}
