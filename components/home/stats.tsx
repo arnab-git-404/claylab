8c3894da9e2a59440c53a44bfdda7a7c02d8907f
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-07-18 07:08:13 +0530

    Update stats.tsx

--- components/home/stats.tsx
+++ components/home/stats.tsx
@@ -24,7 +24,7 @@ export default function Stats() {
     <div className="py-10 sm:py-10 flex flex-col ">
       <div className="w-5xl sm:w-5xl mx-auto flex flex-col sm:flex-row justify-between items-center px-2 sm:px-4 gap-4 sm:gap-0">
         {stats.map((stat, index) => (
-          <div key={index} className="flex flex-col text-center gap-2 sm:gap-6 w-full sm:w-auto transition-transform duration-300 hover:scale-105 hover:shadow-lg cursor-pointer">
+          <div key={index} className="flex flex-col text-center gap-2 sm:gap-6 w-full sm:w-auto transition-transform duration-300 scale-105">
             <div className="text-2xl sm:text-4xl underline">{stat.number}</div>
             <div className="text-lg sm:text-3xl flex flex-col text-center max-w-56 gap-2 sm:gap-6">{stat.label}</div>
           </div>
