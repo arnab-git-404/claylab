7c7bfc8efc73c6345b26fdf5f99ef498d1a189ad
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-08-26 22:29:05 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,20 +2,20 @@
 
 import React from "react";
 import Hero from "@/components/about/hero";
-import Claylab from "@/components/about/claylab";
 import OurJourney from "@/components/about/ourJourney";
 import ReportsSection from "@/components/about/reports";
 import OurTeam from "@/components/about/ourTeam";
 import WhoWeAre from "@/components/about/whoWeAre";
 import WhereWeWork from "@/components/about/whereWeWork";
 
+// import Claylab from "@/components/about/claylab";
 // import StatutoryInfo from "@/components/about/statutoryInformation";
 
 export default function Home() {
   return (
     <div className="flex flex-col max-w-screen bg-clay-light-hover gap-10">
       <Hero />
-      <Claylab />
+      {/* <Claylab /> */}
       <WhoWeAre />
       <OurJourney />
       <WhereWeWork />
