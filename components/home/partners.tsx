4bfcd361faca16b45084f33e4078ba75b128be0d
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-08-27 11:18:46 +0530

    Create partners.tsx

---
+++ components/home/partners.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import Image from "next/image";
+import Heading from "../heading";
+
+interface PartnerImage {
+  imgUrl: string;
+}
+
+const Partners: React.FC = () => {
+  const ImageData: PartnerImage[] = [
+    {
+      imgUrl:
+        "/claylab assets/entrepreneurshipProgram/ourPartners/image 24.png",
+    },
+    {
+      imgUrl:
+        "/claylab assets/entrepreneurshipProgram/ourPartners/image 25.png",
+    },
+    {
+      imgUrl:
+        "/claylab assets/entrepreneurshipProgram/ourPartners/image 26.png",
+    },
+    {
+      imgUrl:
+        "/claylab assets/entrepreneurshipProgram/ourPartners/image 27.png",
+    },
+    {
+      imgUrl:
+        "/claylab assets/entrepreneurshipProgram/ourPartners/image 28.png",
+    },
+    {
+      imgUrl:
+        "/claylab assets/entrepreneurshipProgram/ourPartners/image 29.png",
+    },
+    {
+      imgUrl:
+        "/claylab assets/entrepreneurshipProgram/ourPartners/image 30.png",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col gap-4  max-w-screen z-10 mt-8">
+      <div className="px-4 sm:px-16 ">
+        <Heading text="Our Partners" />
+      </div>
+
+      <div className="overflow-hidden px-4 sm:px-16 ">
+        <div className="flex animate-scroll">
+          {/* First set of images */}
+          {ImageData.map((data: PartnerImage, index: number) => (
+            <Image
+              className="h-16 !w-auto mx-8 flex-shrink-0"
+              key={`${data.imgUrl}-${index}`}
+              src={data.imgUrl}
+              height={60}
+              width={80}
+              alt="partner logo"
+            />
+          ))}
+          {/* Duplicate set for seamless loop */}
+          {ImageData.map((data: PartnerImage, index: number) => (
+            <Image
+              className="h-16 !w-auto mx-8 flex-shrink-0"
+              key={`${data.imgUrl}-duplicate-${index}`}
+              src={data.imgUrl}
+              height={60}
+              width={80}
+              alt="partner logo"
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Partners;
