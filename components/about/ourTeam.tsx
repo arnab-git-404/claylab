1ec9445a433f78538efbc60887af1c830bf6740e
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-08-29 17:25:01 +0530

    Update ourTeam.tsx

--- components/about/ourTeam.tsx
+++ components/about/ourTeam.tsx
@@ -220,19 +220,3 @@ export default function OurTeam() {
 //     const start = currentPage * itemsPerPage;
 //     return team.slice(start, start + itemsPerPage);
 //   };
-
-//   const nextPage = () => {
-//     setCurrentPage((prev) => (prev + 1) % totalPages);
-//   };
-
-//   const prevPage = () => {
-//     setCurrentPage((prev) => (prev - 1 + totalPages) % totalPages);
-//   };
-
-//   return (
-//     <section className="py-16 px-4 md:px-8 lg:px-16 ">
-//       <div className="max-w-7xl mx-auto">
-//         {/* Section Header */}
-//         <div className="text-center mb-16">
-//           <h2 className="text-4xl md:text-5xl font-bold text-gray-900 mb-4">
-//             Meet Our{" "}
