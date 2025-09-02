7c3a360380df35af2a3afd02289eb2093e47b3ad
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-09-02 16:33:09 +0530

    Update hero.tsx

--- components/mentorship/hero.tsx
+++ components/mentorship/hero.tsx
@@ -3,7 +3,8 @@
 "use client";
 
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
+
+// import { Button } from "@/components/ui/button";
 
 export default function Hero() {
   return (
