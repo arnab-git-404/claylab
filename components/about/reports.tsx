72cca42504bba221e5af9a12bcb9fd705d19403f
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-09-20 17:22:52 +0530

    Update reports.tsx

--- components/about/reports.tsx
+++ components/about/reports.tsx
@@ -4,11 +4,8 @@
 //             <div className="flex justify-center mb-6">
 //               <div className="w-8 h-8 sm:w-20 sm:h-20 bg-gray-100 rounded-lg flex items-center justify-center">
 //                 <svg
-//                   className="w-8 h-2 sm:w-10 sm:h-10 text-gray-600"
-//                   fill="none"
-//                   stroke="currentColor"
-//                   viewBox="0 0 24 24"
-//                 >
+
+
 //                   <path
 //                     strokeLinecap="round"
 //                     strokeLinejoin="round"
